2a6a2eb973b21048a584edaa7f7c6aa0b16d3457
Chris Roeder <chris.roeder@cuanschutz.edu>
2020-06-12 18:18:09 +0000

    use all the data, not just test, focus on k=3

--- 03_kmeans.R
+++ 03_kmeans.R
@@ -32,7 +32,10 @@ base_scale=predict(ppr, newdata=base_nmiss)
 
 #base_scale=scale(base_nmiss)
 
-test=data[which(data$test==1),]
+# JUST USE IT ALL
+test=data
+#test=data[which(data$test==1),]
+print(dim(test))
 test_nmiss=test[complete.cases(test[, c(1:3, 5, 6, 9:19, 20:22, 50, 51)]), c(1:3, 5, 6, 9:19, 20:22, 50, 51)]
 test_studyn=test_nmiss$study.1
 test_enrolid=test_nmiss[,1]
@@ -43,25 +46,27 @@ set.seed(123)
 
 kmax=15
 
-# function to compute total within-cluster sum of square 
-wss <- function(k) {
-  kmeans(base_scale, k, iter.max=20, nstart = 4)$tot.withinss
-}
-
-# Compute and plot wss for k = 1 to k = 15
-k.values <- 1:kmax
-
-# extract wss for 1-15 clusters
-wss_values <- map_dbl(k.values, wss)
-
-plot(k.values, wss_values,
-       type="b", pch = 19, frame = FALSE, 
-       xlab="Number of clusters K",
-       ylab="Total within-clusters sum of squares")
+## function to compute total within-cluster sum of square 
+#wss <- function(k) {
+#  kmeans(base_scale, k, iter.max=20, nstart = 4)$tot.withinss
+#}
+#
+## Compute and plot wss for k = 1 to k = 15
+#k.values <- 1:kmax
+#
+## extract wss for 1-15 clusters
+#wss_values <- map_dbl(k.values, wss)
+#
+#plot(k.values, wss_values,
+#       type="b", pch = 19, frame = FALSE, 
+#       xlab="Number of clusters K",
+#       ylab="Total within-clusters sum of squares")
 
 # going to use 3 or 5 clusters
 
 set.seed(123)
+print(dim(test_nmiss))
+print(dim(base_scale))
 cluster1 <- kmeans(base_scale, 3, iter.max=15, nstart = 6)
 cluster2 <- kmeans(base_scale, 5, iter.max=15, nstart = 6)
 
@@ -72,10 +77,21 @@ base_nmiss %>%
   group_by(Cluster) %>%
   summarise_all("mean")
 
-base_nmiss %>%
-  mutate(Cluster = cluster2$cluster) %>%
-  group_by(Cluster) %>%
-  summarise_all("mean")
+#base_nmiss %>%
+#  mutate(Cluster = cluster2$cluster) %>%
+#  group_by(Cluster) %>%
+#  summarise_all("mean")
+
+####polca_formula_3 <- cbind(age, Sex, BMI, Toba, HxMIStr, revasc, LDL, antihyp )~1 
+base_nmiss$Sex <- as.factor(base_nmiss$Sex)
+base_nmiss$Toba <- as.factor(base_nmiss$Toba)
+base_nmiss$HxMIStr <- as.factor(base_nmiss$HxMIStr)
+base_nmiss$revasc <- as.factor(base_nmiss$revasc)
+base_nmiss$antihyp <- as.factor(base_nmiss$antihyp)
+base_nmiss$cluster <- as.factor(cluster1$cluster)
+print(summary(base_nmiss[base_nmiss$cluster == 1,]))
+print(summary(base_nmiss[base_nmiss$cluster == 2,]))
+print(summary(base_nmiss[base_nmiss$cluster == 3,]))
 
 
 # Show how the clusters split out among the studies.
@@ -84,6 +100,8 @@ base_nmiss %>%
 check=data.frame(person_id=enrolid, study=studyn, cluster1=cluster1$cluster, cluster2=cluster2$cluster)
 table(check$study, check$cluster1)
 table(check$study, check$cluster2)
+table(check$study)
+table(check$cluster2)
 
 
 
@@ -117,22 +135,23 @@ predict.kmeans <- function(object,
 test_nmiss2=test_nmiss[, -c(1,7,8,20,21)]
 #test_scale=scale(_nmiss)
 test_scale=predict(ppr, newdata=test_nmiss)
+print("classes")
 pred1=predict.kmeans(cluster1, newdata=test_scale, method="classes")
 pred2=predict.kmeans(cluster2, newdata=test_scale, method="classes")
 std=data.frame(person_id=test_enrolid, study=test_studyn, cluster1=pred1, cluster2=pred2)
-#head(std)
 table(std$study, std$cluster1)
-table(std$study, std$cluster2)
+#table(std$study, std$cluster2)
 
 # predict by centers
+print("centers")
 pred1=predict.kmeans(cluster1, newdata=test_scale, method="centers")
 pred2=predict.kmeans(cluster2, newdata=test_scale, method="centers")
 std=data.frame(person_id=test_enrolid, study=test_studyn, cluster1=pred1, cluster2=pred2)
-#head(std)
 table(std$study, std$cluster1)
-table(std$study, std$cluster2)
+#table(std$study, std$cluster2)
 
-# wtf?
+
+
 #train2=left_join(x=train, y=check, by='person_id')
 #colnames(train)
 #head(train)
