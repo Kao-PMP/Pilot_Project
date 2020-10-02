c8b55b658009ecb281221780ab14085eb35dcea5
Chris Roeder <chris.roeder@ucdenver.edu>
2020-10-02 12:20:06 -0600

    moved Peax to it's own repository

--- Carsten-Peax/peax-pmpTEST/SBP_by_study_forest_plot.R
+++
@@ -1,4 +0,0 @@
-pdf("~/Forest plot mortality vs. 10 mm Hg SBP by study.pdf")
-forestplot(matrix(c("All studies",studies)),mean=study_dth_hrs[,"HR"],lower=study_dth_hrs[,"Lower CI"],upper=study_dth_hrs[,"Upper CI"],
-           zero=1,xticks=c(0.75,0.8,0.9,1,1.1),boxsize = 0.5,col=meta.colors(all.elements="black"),xlab="Hazard ratio/10 mm Hg SBP")
-dev.off()
--- Carsten-Peax/peax-pmpTEST/columnSelection.R
+++
@@ -1,3 +0,0 @@
-data <- read.table('data/short.csv', header=TRUE, na.strings = "NA", sep=",", skipNul = FALSE)
-d_subset = data[,c(1:80)]
-write.csv(d_subset, file="data/shortColumn.csv",row.names=FALSE,quote=FALSE)
